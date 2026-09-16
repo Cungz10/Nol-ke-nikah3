9080b26afca6a1cd2ed207eade97a66a80d74bdc
Cungz10 <rendramukuti10@gmail.com>
2026-09-16 16:01:43 +0700

    ganti style

--- resources/js/components/ui/button/index.ts
+++ resources/js/components/ui/button/index.ts
@@ -9,21 +9,21 @@ export const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground hover:bg-primary/90",
+          "bg-primary text-primary-foreground hover:bg-secondary hover:shadow-level-2 font-bold text-[14px]",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "border-[1.5px] border-primary bg-transparent text-primary hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+          "text-foreground hover:bg-muted dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        "default": "h-9 px-4 py-2 has-[>svg]:px-3",
+        "default": "px-6 py-3 has-[>svg]:px-4 rounded-md",
         "sm": "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        "lg": "h-10 rounded-md px-6 has-[>svg]:px-4",
+        "lg": "h-12 rounded-lg px-8 has-[>svg]:px-6",
         "icon": "size-9",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
